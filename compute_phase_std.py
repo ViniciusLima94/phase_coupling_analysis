e00ccc8958f9c4c5690383c77a4abfd147bc5f1d
vinicius <vinicius.lima.cordeiro@gmail.com>
2025-03-18 09:50:44 +0100

    up

--- compute_phase_std.py
+++ compute_phase_std.py
@@ -18,6 +18,7 @@ parser.add_argument("ALIGN", help="wheter to align data to cue or match", type=s
 parser.add_argument("MONKEY", help="which monkey to use", type=str)
 parser.add_argument("QLOW", help="lower quantile used to threshold", type=int)
 parser.add_argument("QUP", help="upper quantile used to threshold", type=int)
+parser.add_argument("BAND", help="which band to use", type=int)
 parser.add_argument("SURR", help="whether to use the surrogate data", type=int)
 args = parser.parse_args()
 
@@ -27,6 +28,8 @@ at = args.ALIGN
 monkey = args.MONKEY
 q_l = args.QLOW
 q_u = args.QUP
+band = args.BAND
+freq = freqs[band]
 surr = bool(args.SURR)
 
 n_bands = len(freqs)
@@ -61,60 +64,56 @@ if surr:
 
 quantiles = [0.05, 0.5, 0.95]
 
-for band, freq in enumerate(freqs):
+print(f"Band {band + 1} of {n_bands} (f_c = {freq} Hz)")
 
-    print(f"Band {band + 1} of {n_bands} (f_c = {freq} Hz)")
+pec = xr.load_dataarray(
+    os.path.join(data_path, f"burst_trains_band_{band}_{q_l}_{q_u}_surr_{surr}.nc")
+)
 
-    pec = xr.load_dataarray(
-        os.path.join(data_path, f"burst_trains_band_{band}_{q_l}_{q_u}_surr_{surr}.nc")
-    )
+# Load time series of phase differences for data and surrogate
+phi_series = xr.load_dataarray(
+    os.path.join(DATA_PATH, f"phase_difference_time_series_band_{band}_surr_{surr}.nc")
+)
+print(phi_series.dtype)
 
-    # Load time series of phase differences for data and surrogate
-    phi_series = xr.load_dataarray(
-        os.path.join(
-            DATA_PATH, f"phase_difference_time_series_band_{band}_surr_{surr}.nc"
+# Get phase only for coincident events
+filtered_phi_series = xr.DataArray(
+    np.where(~pec, np.nan, phi_series),
+    dims=phi_series.dims,
+    coords=phi_series.coords,
+)
+
+del pec, phi_series
+
+print(filtered_phi_series.shape)
+print(filtered_phi_series.dims)
+
+std = np.stack(
+    [
+        scipy.stats.circstd(
+            filtered_phi_series.isel(roi=i, freqs=0),
+            axis=(0, 1),
+            nan_policy="omit",
         )
-    )
-    print(phi_series.dtype)
+        for i in tqdm(range(filtered_phi_series.sizes["roi"]))
+    ]
+)
 
-    # Get phase only for coincident events
-    filtered_phi_series = xr.DataArray(
-        np.where(~pec, np.nan, phi_series),
-        dims=phi_series.dims,
-        coords=phi_series.coords,
-    )
+std = xr.DataArray(std, dims=("roi",), coords=(filtered_phi_series.roi.values,))
 
-    del pec, phi_series
+# if surr:
+#    temp_cl = []
+#    for q in quantiles:
+#        temp_cl += [std.quantile(q, dim="roi")]
+#    std = xr.concat(temp_cl, "quantiles")
 
-    print(filtered_phi_series.shape)
-    print(filtered_phi_series.dims)
+###########################################################################
+# Saves file
+###########################################################################
+pec_file_name_cl = f"phase_std_band_{band}_{q_l}_{q_u}_surr_{surr}.nc"
+std.to_netcdf(
+    os.path.join(results_path, pec_file_name_cl),
+    engine="h5netcdf",
+)
 
-    std = np.stack(
-        [
-            scipy.stats.circstd(
-                filtered_phi_series.isel(roi=i, freqs=0),
-                axis=(0, 1),
-                nan_policy="omit",
-            )
-            for i in tqdm(range(filtered_phi_series.sizes["roi"]))
-        ]
-    )
-
-    std = xr.DataArray(std, dims=("roi",), coords=(filtered_phi_series.roi.values,))
-
-    if surr:
-        temp_cl = []
-        for q in quantiles:
-            temp_cl += [std.quantile(q, dim="roi")]
-        std = xr.concat(temp_cl, "quantiles")
-
-    ###########################################################################
-    # Saves file
-    ###########################################################################
-    pec_file_name_cl = f"phase_std_band_{band}_{q_l}_{q_u}_surr_{surr}.nc"
-    std.to_netcdf(
-        os.path.join(results_path, pec_file_name_cl),
-        engine="h5netcdf",
-    )
-
-    del std
+del std
